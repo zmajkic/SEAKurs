7875e6415090fbec3b5a22a864d05d9957319837
zmajkic <zeljko.majkic@telekom.de>
2021-04-27 13:25:30 +0200

    public Object getRootApp

--- src/main/java/de/telekom/sea/Main.java
+++ src/main/java/de/telekom/sea/Main.java
@@ -3,10 +3,14 @@ package de.telekom.sea;
 import de.telekom.sea.seminar.SeminarApp;
 
 public class Main {
-
+			
 	public static void main(String[] args) {
-
+        
 		SeminarApp app = new SeminarApp();
+		var app2 = app.getRootApp();
+		System.out.println(app);
+		System.out.println(app2);
+		app.setParent(null);
 		app.run(args);
 
 	}
--- src/main/java/de/telekom/sea/seminar/SeminarApp.java
+++ src/main/java/de/telekom/sea/seminar/SeminarApp.java
@@ -14,9 +14,13 @@ public class SeminarApp {
 
 	public void run(String[] args) {
 		Person child = new Person();
-		child.setParent(this);  // create a child in this Method
+		child.setParent(this); // create a child in this Method
 
 		System.out.println("Hello World");
+	}
+
+	public Object getRootApp() {
+		return this;
 	}
 
 }
